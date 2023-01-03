71c49311b24256cffbbc99c9df1e8765ab00476e
kuldeepSingh <singhkuldeep7850@gmail.com>
2023-01-03 22:34:06 +0530

    accordian added

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import * as React from "react";
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { ContentPlaceholder } from "./ContentPlaceholder";
 
-const Footer = () => {
+const Footer = ({ i, expanded, setExpanded }) => {
+  const isOpen = i === expanded;
+
   return (
-    <div>Footer</div>
-  )
-}
+    <>
+      <motion.header
+        initial={false}
+        animate={{ backgroundColor: isOpen ? "#FF0088" : "#0055FF" }}
+        onClick={() => setExpanded(isOpen ? false : i)}
+      />
+      <AnimatePresence initial={false}>
+        {isOpen && (
+          <motion.section
+            key="content"
+            initial="collapsed"
+            animate="open"
+            exit="collapsed"
+            variants={{
+              open: { opacity: 1, height: "auto" },
+              collapsed: { opacity: 0, height: 0 }
+            }}
+            transition={{ duration: 0.8, ease: [0.04, 0.62, 0.23, 0.98] }}
+          >
+            <ContentPlaceholder />
+          </motion.section>
+        )}
+      </AnimatePresence>
+    </>
+  );
+};
 
-export default Footer
+export const Example = () => {
+  const [expanded, setExpanded] = useState(0);
+
+  return FooterIds.map((i) => (
+    <Footer i={i} expanded={expanded} setExpanded={setExpanded} />
+  ));
+};
+
+const FooterIds = [0, 1, 2, 3];
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,9 @@ import Header from '../components/header/Header'
 import { Fade } from 'react-awesome-reveal'
 import { Link } from 'react-router-dom'
 import './home.css'
+import Info from '../components/info/Info'
+import Heading from '../components/heading/Heading'
+import {Example} from '../components/footer/Footer'
 
 const Home = () => {
   return (
@@ -45,7 +48,7 @@ const Home = () => {
         <Fade>
         <div className='home' >
        
-              <div className='Home_left' style={{backgroundColor: '#006241', marginTop:'30px 0px'}}>
+              <div className='Home_left' style={{backgroundColor: '#006241', margin:'30px 0px 0px 0px'}}>
               <img
               className='burgur'
               src="https://content-prod-live.cert.starbucks.com/binary/v2/asset/137-82316.png"
@@ -53,7 +56,7 @@ const Home = () => {
             />
               </div>
 
-              <div className='home_right_second' style={{padding:'50px 0px' , backgroundColor: '#006241'}}>
+              <div className='home_right_third'>
                   <h2>Warm and hearty</h2>
                   <p>The Impossible™ Breakfast Sandwich will keep you going with an Impossible Sausage patty made from plants, egg and Cheddar.*.</p>
                   <a>Order now</a>
@@ -62,6 +65,32 @@ const Home = () => {
           </div>
         </Fade>
 
+        <Fade>
+        <Heading heading="MORE TO DISCOVER" />
+      </Fade>
+      <Fade>
+        <div className="homeScreen__discover">
+          <Info
+            title="Order and pick up. Easy as that."
+            image="https://content-prod-live.cert.starbucks.com/binary/v2/asset/137-67828.jpg"
+            info="Just open the app, order your favorites, and enjoy contactless pay. From there, choose whichever pickup method is best for you."
+            link="See pickup options"
+            color="#1e3932"
+            background="#d4e9e4"
+            className="info__hoverLight"
+          />
+          <Info
+            title="Coffee delivered. Day made."
+            image="https://content-prod-live.cert.starbucks.com/binary/v2/asset/137-67829.jpg"
+            info="Make everything a little brighter. Enjoy a $0 Delivery Fee* on your first Uber Eats order over $15 through 1/31."
+            link="Order now"
+            color="#1e3932"
+            background="#d4e9e4"
+            className="info__hoverLight"
+          />
+        </div>
+      </Fade>
+      <Example/>
       </div>
     </div>
   )
